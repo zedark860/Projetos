6565e6e91145cb81623d767d63a65974c385b57d
Davi <oversouls11@gmail.com>
2024-02-09 22:35:29 -0300

    Atualizando projeto

--- Projeto Automação Contra Cheque/contracheque.py
+++ Projeto Automação Contra Cheque/contracheque.py
@@ -15,13 +15,9 @@ url_site = 'https://assinarweb.com.br/arweb/login'
 
 login = ''
 senha = ''
-# Obtém o diretório do usuário da máquina atual
-user_directory = os.path.expanduser("~")
-# Cria o caminho completo para o diretório de PDFs
-diretorio_pdf = os.path.join(user_directory, 'Desktop', 'ContraCheque', 'PDF')
+diretorio_pdf = r''
 arquivos_pdf = glob.glob(os.path.join(diretorio_pdf, '*.pdf'))
 arquivos = os.listdir(diretorio_pdf)
-
 
 def ler_planilha(planilha_path, coluna_nome):
     try:
@@ -43,12 +39,8 @@ def ler_planilha(planilha_path, coluna_nome):
 
 
 def encontrar_pdf(nome):
-    # Obtém o diretório do usuário da máquina atual
-    user_directory = os.path.expanduser("~")
-
-    diretorio_pdfs = os.path.join(
-        user_directory, 'Desktop', 'ContraCheque', 'PDF')
-    nome = f"{nome}"
+    diretorio_pdfs = r''
+    nome = nome.strip() # Remover espaços extras no início e no final do nome
     extensao = '.pdf'
 
     caminho_completo = os.path.join(diretorio_pdfs, f"{nome}{extensao}")
@@ -61,38 +53,17 @@ def encontrar_pdf(nome):
         return None
 
 
-# Exemplo de uso
-try:
-    # Obtém o diretório do usuário da máquina atual
-    user_directory = os.path.expanduser("~")
-
-    # Cria o caminho completo para a planilha
-    planilha_path = os.path.join(
-        user_directory, 'Desktop', 'ContraCheque', 'Funcionários2024.xlsx')
-    coluna_nome = 'NOME'
-
-    if not os.path.exists(planilha_path):
-        raise FileNotFoundError(
-            f"Arquivo da planilha não encontrado: {planilha_path}")
-
-    df_contatos = ler_planilha(planilha_path, coluna_nome)
-
-    # Agora você tem um DataFrame contendo as colunas 'NOME' e 'ARQUIVOS' sem valores nulos
-    print(df_contatos)
-except Exception as e:
-    print(f"Erro: {str(e)}")
-
 # Define a função para escrever no log
-
-
 def escrever_log(mensagem):
     with open("enviados.txt", "a") as log_file:
         log_file.write(f"{mensagem}\n")
+    print(f"Log escrito: {mensagem}")
 
 
 def escrever_erro(mensagem):
     with open("erros.txt", "a") as log_file:
         log_file.write(f"{mensagem}\n")
+    print(f"Erro escrito: {mensagem}")   
 
 
 def enviar_contracheques(login, senha, contatos):
@@ -101,9 +72,7 @@ def enviar_contracheques(login, senha, contatos):
     chrome_options.add_argument('--start-maximized')
 
     # Configuração do caminho do driver do Chrome
-    user_dir = os.path.expanduser("~")
-    webdriver_path = os.path.join(
-        user_dir, 'Desktop', 'ContraCheque', 'chrome', 'chromedriver.exe')
+    webdriver_path = './chrome/chromedriver.exe'
 
     # Adicione o caminho do driver ao PATH
     os.environ['PATH'] = f'{os.environ["PATH"]};{os.path.abspath(webdriver_path)}'
@@ -123,121 +92,210 @@ def enviar_contracheques(login, senha, contatos):
             (By.NAME, 'nome'))).send_keys(Keys.RETURN)
 
         # Itera sobre os colaboradores ativos e realiza a automação no site
-        for _, row in contatos.iterrows():
+        for _ , row in contatos.iterrows():
             nome_colaborador = row["NOME"]
             print(f"Processando: {nome_colaborador}")
 
-            # Clique no primeiro botão
-            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                (By.CLASS_NAME, 'm-widget4__ext')))
-            element.click()
+            try:
+                time.sleep(3)
+                
+                # Clique no botão Novo Documento
+                element = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/div[1]/div[1]/a/div')))
+                element.click()
+                
+                time.sleep(3)
 
-            # Clique no segundo botão
-            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="m_wizard_form_step_1"]/div/div[2]/div[2]/label/span[1]/span/span')))
-            element.click()
+                # Clique no botão Assinatura Digital
+                element = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="m_wizard_form_step_1"]/div/div[2]/div[2]/label')))
+                element.click()
+                
+                time.sleep(3)
 
-            time.sleep(5)
+                # Abre a tela de seleção de arquivo no navegador
+                element = WebDriverWait(driver, 15).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="mDropzoneEnvelope"]')))
+                element.click()
+                
+                time.sleep(3)
 
-            # Abre a tela de seleção de arquivo no navegador
-            element = WebDriverWait(driver, 15).until(
-                EC.presence_of_element_located((By.ID, 'mDropzoneEnvelope')))
-            element.click()
+                # Encontra o caminho do arquivo PDF usando a função encontrar_pdf
+                caminho_pdf = encontrar_pdf(nome_colaborador)
 
-            # Encontra o caminho do arquivo PDF usando a função encontrar_pdf
-            caminho_pdf = encontrar_pdf(nome_colaborador)
+                print(f"Caminho do PDF para {nome_colaborador}: {caminho_pdf}")
 
-            print(f"Caminho do PDF para {nome_colaborador}: {caminho_pdf}")
+                if caminho_pdf:
+                        # Clique na caixa de diálogo para torná-la ativa
+                        pyautogui.FAILSAFE = False
 
-            time.sleep(5)
+                        # Adicione um atraso de 3 segundos antes de escrever
+                        time.sleep(3)
 
-            if caminho_pdf:
-                try:
-                    # Clique na caixa de diálogo para torná-la ativa
-                    pyautogui.FAILSAFE = False
+                        # Digite o caminho completo do arquivo usando pyautogui
+                        pyautogui.write(caminho_pdf)
 
-                    # Digite o caminho completo do arquivo usando pyautogui
-                    pyautogui.write(caminho_pdf)
+                        # Pressione Enter para confirmar a seleção
+                        pyautogui.press('enter')
 
-                    # Pressione Enter para confirmar a seleção
-                    pyautogui.press('enter')
+                        # Aguarde alguns segundos para o processo de seleção ser concluído
+                        time.sleep(3)
 
-                    time.sleep(5)
+                        # Clica no Botão Avançar
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_form"]/div[2]/div/div/div[2]/a[2]'))).click()
 
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="m_form"]/div[2]/div/div/div[2]/a[2]/span'))).click()
+                        time.sleep(3)
+                        
+                        # Clica no botão Buscar Participante Cadastrado
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_wizard_form_step_2"]/div/button[2]'))).click()
+                        
+                        time.sleep(5)
+                        
+                        # Clica no botão Pesquisar
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="tblBuscarParticipantes_filter"]/label/input'))).click()
+                        
+                        time.sleep(5)
+                        # Pega o campo de filtro e armazena em uma variável
+                        search_input = driver.find_element(
+                            By.XPATH, '//*[@id="tblBuscarParticipantes_filter"]/label/input')
+                        search_input.click()
 
-                    time.sleep(5)
+                        # insere o nome na caixa de texto
+                        search_input.send_keys(nome_colaborador)
 
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="m_wizard_form_step_2"]/div/button[2]'))).click()
+                        # Pressiona Enter para executar a pesquisa
+                        search_input.send_keys(Keys.RETURN)
 
-                    time.sleep(5)
+                        # Aguarde a página carregar (ajuste conforme necessário)
+                        time.sleep(3)
+                        
+                        # Pega o primeiro nome encontrado na coluna
+                        nome_encontrado = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="tblBuscarParticipantes"]/tbody/tr/td[2]'))).text
 
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="tblBuscarParticipantes_filter"]/label/input'))).click()
+                        # Verifica se o nome encontrado na tabela é diferente do nome do colaborador
+                        if nome_encontrado.strip() != nome_colaborador:
+                            print(f"O nome encontrado ({nome_encontrado}) é diferente do nome do colaborador ({nome_colaborador}). Pulando para o próximo colaborador.")
+                            
+                            # Clica no botão para fechar a tela de opção que está na frente da principal
+                            WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                                        (By.XPATH, 'id("addParticipante")/DIV[1]/DIV[1]/DIV[1]/BUTTON[1]'))).click()
+                            
+                            time.sleep(3)
+                            
+                            # Clica na imagem da página e volta para página inicial
+                            WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                                        (By.XPATH, '//*[@id="m_header"]/div[1]/div/div/div[1]/div/div[1]/a/img'))).click()
+                            
+                            time.sleep(3)
+                            
+                            continue
+                        
+                        # Clica no botão de ação
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, 'id("tblBuscarParticipantes")/TBODY[1]/TR[1]/TD[5]/A[1]'))).click()
+                        
+                        time.sleep(3)
+                        
+                        # Clica no botão adicionar
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, 'id("adicionarNovo")/DIV[1]/DIV[13]/BUTTON[1]'))).click()
+                        
+                        time.sleep(3)
+                        
+                        # Clica no botão avançar
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_form"]/div[2]/div/div/div[2]/a[2]'))).click()
 
-                    time.sleep(5)
+                        time.sleep(5)
 
-                    # Clique no campo de filtro
-                    search_input = driver.find_element(
-                        By.XPATH, '//*[@id="tblBuscarParticipantes_filter"]/label/input')
-                    search_input.click()
+                        # Clica no botão de colocar assinatura no arquivo
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="participant_list"]/div/div/div/div[3]/label[1]/span'))).click()
+                        
+                        time.sleep(3)
+                        
+                        # Clica em salvar o arquivo
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="salvar"]'))).click()
 
-                    # ira o nome na caixa de texto
-                    search_input.send_keys(nome_colaborador)
+                        time.sleep(5)
+                        
+                        # Clica em publicar o arquivo
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '/html/body/div[5]/div/div[3]/button[1]'))).click()
 
-                    # Pressiona Enter para executar a pesquisa
-                    search_input.send_keys(Keys.RETURN)
+                        time.sleep(5)
 
-                    # Aguarde a página carregar (ajuste conforme necessário)
-                    time.sleep(5)
+                        quantidade += 1
 
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="tblBuscarParticipantes"]/tbody/tr/td[5]/a'))).click()
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="adicionarNovo"]/div[1]/div[13]/button'))).click()
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="m_form"]/div[2]/div/div/div[2]/a[2]'))).click()
+                        # Clica na imagem da página e volta para página inicial
+                        WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_header"]/div[1]/div/div/div[1]/div/div[1]/a/img'))).click()
 
-                    time.sleep(5)
+                        data = datetime.datetime.now()
+                        data_formatada = data.strftime('%H:%M:%S %d/%m/%y')
 
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="participant_list"]/div/div/div/div[3]/label[1]'))).click()
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '//*[@id="salvar"]'))).click()
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '/html/body/div[5]/div/div[3]/button[1]'))).click()
-
-                    quantidade += 1
-
-                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-                        (By.XPATH, '/html/body/div[8]/div/div[3]/button[1]'))).click()
-
-                    time.sleep(5)
-
-                    data = datetime.datetime.now()
-                    data_formatada = data.strftime('%H:%M:%S %d/%m/%y')
-
-                    escrever_log(
-                        f"Mensagem Contra Cheque enviado para {nome_colaborador} às {data_formatada}")
-
-                    print(
-                        f"Mensagem Contra Cheque enviado para {nome_colaborador} às {data_formatada}")
-
-                except Exception as ex:
+                        escrever_log(
+                            str(f"Mensagem Contra Cheque enviado para {nome_colaborador} às {data_formatada}"))
+                        
+                else:
+                    # Se o arquivo não foi encontrado, escreva no log de erros e continue para o próximo colaborador
                     dataError = datetime.datetime.now()
-                    dataError_formatada = dataError.strftime(
-                        '%H:%M:%S %d/%m/%y')
-                    # Em caso de outras exceções, escreve no log
+                    dataError_formatada = dataError.strftime('%H:%M:%S %d/%m/%y')
                     escrever_erro(
-                        f"ERRO: {str(ex)} durante o processamento do arquivo {nome_colaborador} às {dataError_formatada}")
-
-                    print(
-                        f"ERRO: {str(ex)} durante o processamento do arquivo {nome_colaborador} às {dataError_formatada}")
+                        str(f"ERRO: Arquivo não encontrado para {nome_colaborador} às {dataError_formatada}"))
                     
+                    # Se ocorrer um erro, feche a janela de seleção de arquivo
+                    pyautogui.hotkey('esc')
+                    
+                    time.sleep(3)
+                    
+                    # Clica na imagem da página e volta para página inicial
+                    WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_header"]/div[1]/div/div/div[1]/div/div[1]/a/img'))).click()
+                    
+                    time.sleep(3)
+                    
+                    # Continue para o próximo colaborador mesmo em caso de erro
                     continue
-    driver.quit()        
+        
+            except Exception as e:
+                # Em caso de outras exceções, escreve no log
+                dataError = datetime.datetime.now()
+                dataError_formatada = dataError.strftime(
+                        '%H:%M:%S %d/%m/%y')
+                escrever_erro(
+                    str(f"ERRO ao Processar: durante o processamento do arquivo {nome_colaborador} às {dataError_formatada}"))
+                
+                # Clica na imagem da página e volta para página inicial
+                WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="m_header"]/div[1]/div/div/div[1]/div/div[1]/a/img'))).click()
+                
+                time.sleep(3)
+                
+                # Continue para o próximo colaborador mesmo em caso de erro
+                continue
+
+
+# Exemplo de uso
+try:
+    planilha_path = './Gestão de Funcionários 2023.xlsx'
+    coluna_nome = 'NOME'
+
+    if not os.path.exists(planilha_path):
+        raise FileNotFoundError(
+            f"Arquivo da planilha não encontrado: {planilha_path}")
+
+    df_contatos = ler_planilha(planilha_path, coluna_nome)
+
+    # Agora você tem um DataFrame contendo as colunas 'NOME' e 'ARQUIVOS' sem valores nulos
+    print(df_contatos)
+except Exception as e:
+    print(f"Erro: {str(e)}")
 
 # Chamada da função com os novos valores de login e senha
 enviar_contracheques(login, senha, df_contatos)
